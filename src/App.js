a6f020d25a1eac951c2c8251495a3540a328d70a
EricSparks <sparkseric90@gmail.com>
2021-04-02 10:00:37 -0400

    functioning, added images under to each menu section and added chef info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import './App.css';
 import Logo from "./logo.png"
+import Fancy from './fancy.jpeg';
+import Lunch from './lunch.jpg';
+import Dinner from './dinner.jpg';
+import Dessert from './dessert.jpg';
+import Chef from './chef.jpg';
 
 
 
@@ -17,7 +22,7 @@ export default class App extends Component {
     }
   }
 
-  
+
 
   componentDidUpdate() {
     localStorage.setItem('Appetizers', JSON.stringify(this.state.Appetizers))
@@ -105,16 +110,16 @@ export default class App extends Component {
 
     let myItems = [];
 
-    if(this.state.Drinks != null){
+    if (this.state.Drinks != null) {
       myItems = this.state.Drinks;
     }
-    
+
     return (
       <div className="container-fluid text-center">
 
 
 
-<div className="row">
+        <div className="row">
           <div className="col-12 mx-auto">
           </div>
         </div>
@@ -125,7 +130,7 @@ export default class App extends Component {
             <div className="col">
               <div class="card">
                 <div className="card-body2">
-                <img className="card-img-top" src={Logo} />
+                  <img className="card-img-top" src={Logo} />
                 </div>
               </div>
             </div>
@@ -137,7 +142,6 @@ export default class App extends Component {
 
 
         <button class="btn btn-info" type="button" data-bs-toggle="offcanvas" data-bs-target="#app" aria-controls="offcanvasExample">Apps</button>
-
         <div class="offcanvas offcanvas-start" tabindex="-1" id="app" aria-labelledby="offcanvasExampleLabel">
           <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="app">Appetizers</h5>
@@ -145,6 +149,7 @@ export default class App extends Component {
           </div>
           <div class="offcanvas-body">
             <div className="card-body">
+            <img className="card-img-top" src={Fancy} />
               <ul className="list-unstyled">
                 {this.state.Appetizers.map(appetizer => {
                   return (<li>
@@ -161,7 +166,6 @@ export default class App extends Component {
 
 
         <button class="btn btn-info" type="button" data-bs-toggle="offcanvas" data-bs-target="#lunch" aria-controls="offcanvasExample">Lunch</button>
-
         <div class="offcanvas offcanvas-start" tabindex="-1" id="lunch" aria-labelledby="offcanvasExampleLabel">
           <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="lunch">Lunch</h5>
@@ -169,6 +173,7 @@ export default class App extends Component {
           </div>
           <div class="offcanvas-body">
             <div className="card-body">
+            <img className="card-img-top" src={Lunch} />
               <ul className="list-unstyled">
                 {this.state.Lunch.map(lunch => {
                   return (<li>
@@ -207,7 +212,6 @@ export default class App extends Component {
 
 
         <button class="btn btn-info" type="button" data-bs-toggle="offcanvas" data-bs-target="#dinner" aria-controls="offcanvasRight">Dinner</button>
-
         <div class="offcanvas offcanvas-end" tabindex="-1" id="dinner" aria-labelledby="offcanvasRightLabel">
           <div class="offcanvas-header">
             <h5 id="offcanvasRightLabel">Dinner</h5>
@@ -215,6 +219,7 @@ export default class App extends Component {
           </div>
           <div class="offcanvas-body">
             <div className="card-body">
+            <img className="card-img-top" src={Dinner} />
               <ul className="list-unstyled">
                 {this.state.Dinner.map(dinner => {
                   return (<li>
@@ -233,7 +238,6 @@ export default class App extends Component {
 
 
         <button class="btn btn-info" type="button" data-bs-toggle="offcanvas" data-bs-target="#dessert" aria-controls="offcanvasRight">Dessert</button>
-
         <div class="offcanvas offcanvas-end" tabindex="-1" id="dessert" aria-labelledby="offcanvasRightLabel">
           <div class="offcanvas-header">
             <h5 id="offcanvasRightLabel">Dessert</h5>
@@ -241,6 +245,7 @@ export default class App extends Component {
           </div>
           <div class="offcanvas-body">
             <div className="card-body">
+            <img className="card-img-top" src={Dessert} />
               <ul className="list-unstyled">
                 {this.state.Dessert.map(dessert => {
                   return (<li>
@@ -256,33 +261,6 @@ export default class App extends Component {
         </div>
 
 
-
-
-
-        <div className="text-black">
-          <h1>Hours of Operation</h1>
-          <p>Sunday - Saturday</p>
-          <p>8am - 9pm</p>
-          <h1>Location</h1>
-          <p>348 E Main St</p>
-          <p>Lexington, Ky</p>
-        </div>
-
-      </div>
-
-    )
-  }
-}
-
-
-/*
-
-
-
-
-
-<div className="container-full text-center">
-
         <div className="row">
           <div className="col-12 mx-auto">
           </div>
@@ -293,9 +271,12 @@ export default class App extends Component {
             </div>
             <div className="col">
               <div class="card">
-                <div className="card-body">
-                  <h5 className="card-title">Test</h5>
-                  <p className="card-text">Test</p>
+                <div className="card-body2">
+                  <img className="card-img-top" src={Chef} />
+                  <h1>Head Chef:</h1>
+                  <p>Mr. Bob Squarepants</p>
+                  <p>Bobs been a fry cook for 17 years for a world famous burger shop. He recently moved into the fancy diner world were he hopes to make a
+                  successful splash.</p>
                 </div>
               </div>
             </div>
@@ -303,128 +284,25 @@ export default class App extends Component {
             </div>
           </div>
         </div>
-        <div class="accordion" id="accordionExample">
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                Accordion Item #1
-      </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <ul className="list-unstyled">
-                  {this.state.Appetizers.map(appetizer =>
-                    <div>
-                      <h1>{appetizer.name.split(" with ")[0]}</h1>
-                      <li>{appetizer.description}</li>
-                      <p>{appetizer.description.length}$</p>
-                    </div>
-                  )}
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingTwo">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Accordion Item #2
-      </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-            </div>
-          </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingThree">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Accordion Item #3
-      </button>
-            </h2>
-            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-            </div>
+
+        <div className="card-body">
+          <div className="text-black">
+            <h1>Hours of Operation</h1>
+            <p>Sunday - Saturday</p>
+            <p>8am - 9pm</p>
+            <h1>Location</h1>
+            <p>348 E Main St</p>
+            <p>Lexington, Ky</p>
           </div>
         </div>
-
-        <div className="text-black">
-          <h1>Hours of Operation</h1>
-          <p>Sunday - Saturday</p>
-          <p>8am - 9pm</p>
-          <h1>Location</h1>
-          <p>348 E Main St</p>
-          <p>Lexington, Ky</p>
-        </div>
       </div>
 
-        <div class="accordion" id="accordionExample">
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Appetizers
-      </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <div className="card-body">
-                  <ul className="list-unstyled">
-                    {this.state.Appetizers.map(appetizer => {
-                      return (<li>
-                        <h1>{appetizer.name.split(" with ")[0]}</h1>
-                        <p>{appetizer.description}</p>
-                        <p>{appetizer.description.length}$</p>
-                      </li>)
-                    }
-                    )}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingTwo">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Lunch
-      </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <div className="card-body">
-                  <ul className="list-unstyled">
-                    {this.state.Lunch.map(lunch => {
-                      return (<li>
-                        <h1>{lunch.name.split(" with ")[0]}</h1>
-                        <p>{lunch.description}</p>
-                        <p>{lunch.description.length}$</p>
-                      </li>)
-                    }
-                    )}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="headingThree">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Accordion Item #3
-      </button>
-            </h2>
-            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-            </div>
-          </div>
-        </div>
+
+    )
+  }
+}
 
 
 
 
 
-
-
-*/
